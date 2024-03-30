9303ba5593f6f30b39dd483cb1444c7b5dd4d5cf
crypter70 <yosafatcrypter@gmail.com>
2024-03-30 21:55:47 +0700

    Update App.py

--- App.py
+++ App.py
@@ -34,7 +34,7 @@ def main():
     example_list = ["Doi asik bgt orangnya", "Ada pengumuman nih gaiss, besok liburr", "Kok gitu sih kelakuannya"]
     options = example_list + ["Input a new text ..."]
 
-    selection = st.selectbox("Example Text", options=options)
+    selection = st.selectbox("Examples Text", options=options)
 
     text = ""
 
